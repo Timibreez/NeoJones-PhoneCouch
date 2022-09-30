704c0c3e0e4004f0195bf84a718c7fde603a32e0
Timibreez <ffftimi@gmail.com>
2022-09-30 11:50:59 +0100

    Created a Phone model, and added a phone with the view

---
+++ PhoneCouch/Controllers/PhonesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneCouch.Models;
+
+namespace PhoneCouch.Controllers
+{
+    public class PhonesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var phone = new Phone() { Name = "X" };
+            return View(phone);
+        }
+    }
+}
