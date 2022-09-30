048b74929692120b5f9b9cf97d78a2430d4e33d8
Timibreez <ffftimi@gmail.com>
2022-09-30 10:49:40 +0100

    initial commit

---
+++ PhoneCouch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PhoneCouch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
