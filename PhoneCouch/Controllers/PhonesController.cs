630368d3355162dcf09459589ff7e4a58a165fc1
Timibreez <ffftimi@gmail.com>
2022-10-04 11:09:04 +0100

    added index function to the PhonesController

--- PhoneCouch/Controllers/PhonesController.cs
+++ PhoneCouch/Controllers/PhonesController.cs
@@ -5,6 +5,7 @@ namespace PhoneCouch.Controllers
 {
     public class PhonesController : Controller
     {
+        // Random IPhones
         public IActionResult Random()
         {
             var phone = new Phone() { Name = "X" };
@@ -12,7 +13,25 @@ namespace PhoneCouch.Controllers
             //return Content("Hello world!");
             //return NotFound();
             //return new EmptyResult();
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            return View(phone);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return Content("Id = " + id);
+        }
+
+        // list of Available Phones
+        public IActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy)){
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageindex= {0} & sortBy= {1}", pageIndex, sortBy));
         }
     }
 }
